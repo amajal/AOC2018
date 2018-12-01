0fcbff6f39796e7fa42d9256574d459d4f6f0bab
Ali <alihudaifa@gmail.com>
2018-12-01 14:44:55 +0800

    Day 1 of AOC 2018

---
+++ src/Day1.kt
@@ -0,0 +1,41 @@
+import java.io.File
+import java.io.InputStream
+import kotlin.system.measureTimeMillis
+
+fun main(args: Array<String>) {
+    val inputStream: InputStream = File("Input1.txt").inputStream()
+    val listOfNumbers = mutableListOf<Int>()
+    val setOfTotals = mutableSetOf<Int>()
+    var total = 0
+
+    inputStream.bufferedReader().useLines { lines ->
+        lines.forEach {
+            total += it.toInt()
+            listOfNumbers.add(it.toInt())
+        }
+    }
+
+    println("total is $total")
+
+    var index = 0
+    total = 0
+
+    println("Calculating repeat frequency ...")
+    val time = measureTimeMillis {
+        while (true) {
+            total += listOfNumbers[index]
+
+            if (total in setOfTotals) {
+                println("Repeat at $total")
+                break
+            }
+
+            setOfTotals.add(total)
+
+            if (++index == listOfNumbers.size)
+                index = 0
+        }
+    }
+
+    println("time taken to calculate repeat $time ms")
+}
