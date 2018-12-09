dd8d3733f773fbdb0515e7a9aeb677acaf2679e8
Ali <alihudaifa@gmail.com>
2018-12-09 17:25:06 +0800

    Day 9 of AOC 2018 .. part 2

--- src/Day9.kt
+++ src/Day9.kt
@@ -1,10 +1,9 @@
 fun main(args: Array<String>) {
-    println("Part 1 -> ${findMaxScore(452, 71250)}")
+    println("Part 1 -> ${findMaxScore(452, 71250)} ")
+    println("Part 2 -> ${findMaxScoreOptimised(452, 71250 * 100)}")
 }
 
-
-fun findMaxScore(players:Int, marbles:Int): Int
-{
+fun findMaxScore(players: Int, marbles: Int): Int {
     val score = Array(players) { 0 }
     val circle = mutableListOf(0)
     var currentPlayer = 0
@@ -21,16 +20,49 @@ fun findMaxScore(players:Int, marbles:Int): Int
         }
 
         currentPlayer = (currentPlayer + 1) % score.size
-
-        /*print ("[$i]")
-        for (j in 0 until circle.size) {
-            if (currentIndex == j)
-                print(" (${circle[j]})")
-            else
-                print(" ${circle[j]}")
-        }
-        println()*/
     }
 
     return score.max()!!
+}
+
+
+fun findMaxScoreOptimised(players: Int, marbles: Int): Long {
+    val score = Array<Long>(players) { 0 }
+    var current = Node(0)
+    var currentPlayer = 0
+
+    for (i in 1..marbles) {
+        if (i % 23 == 0) {
+            repeat(7) {
+                current = current.left
+            }
+            score[currentPlayer] += (i + current.id).toLong()
+            current.left.right = current.right
+            current.right.left = current.left
+            current = current.right
+        } else {
+            current = current.right
+            val newNode = Node(i)
+
+            newNode.left = current
+            newNode.right = current.right
+            current.right.left = newNode
+            current.right = newNode
+            current = newNode
+        }
+
+        currentPlayer = (currentPlayer + 1) % score.size
+    }
+
+    return score.max()!!
+}
+
+class Node() {
+    var id: Int = 0
+    var left: Node = this
+    var right: Node = this
+
+    constructor(id: Int) : this() {
+        this.id = id
+    }
 }
