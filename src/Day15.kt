d0bc009bd6d474c8b612c35d6bd8b79c2a8d5bdd
Ali <alihudaifa@gmail.com>
2018-12-15 20:39:57 +0800

    Day 15 of AOC

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,11 +1,86 @@
 import java.io.File
 
-fun main (args: Array<String>){
+fun main(args: Array<String>) {
     val input = File("Demo.txt").readLines()
-    val battleGround = Array(input.size) {charArrayOf('.')}
-    for (i in 0 until input.size)
-        battleGround[i] = input[i].toCharArray()
 
+    val result = initialiseBoard(input)
+    val board = result.first
+    val pieces = result.second
 
-    battleGround.forEach { println(it) }
+    print(board, pieces)
+
+    repeat(1)
+    {
+        pieces.forEach { currentPiece ->
+            if (currentPiece.HP > 0) {
+                val enemies = pieces.filter { p -> p.HP > 0 && p.Type != currentPiece.Type }
+                move(currentPiece, enemies)
+                attack(currentPiece, enemies)
+            }
+        }
+
+        pieces.removeAll { p -> p.HP < 1 }
+        pieces.sortBy { p -> p.Row }
+        pieces.sortBy { p -> p.Col }
+    }
 }
+
+fun move(currentPiece: Piece, enemies: List<Piece>) {
+
+}
+
+fun attack(currentPiece: Piece, enemies: List<Piece>) {
+    val enemiesInRange = enemies.filter { p ->
+        (Math.abs(p.Col - currentPiece.Col) == 1 && p.Row == currentPiece.Row) ||
+                (Math.abs(p.Row - currentPiece.Row) == 1 && p.Col == currentPiece.Col)
+    }
+
+    if (enemiesInRange.count() == 0)
+        return
+
+    val weakestEnemyInRange = enemiesInRange
+            .sortedBy { p -> p.HP }
+            .sortedBy { p -> p.Row }
+            .sortedBy { p -> p.Col }
+            .first()
+
+    weakestEnemyInRange.HP -= 3
+
+}
+
+
+fun print(board: MutableList<MutableList<Char>>, pieces: MutableList<Piece>) {
+    for (i in 0 until board.size) {
+        for (j in 0 until board[i].size) {
+            val matchPiece = pieces.filter { p -> p.Row == i && p.Col == j }
+            if (matchPiece.count() == 0)
+                print(board[i][j])
+            else
+                print(matchPiece.first().Type)
+        }
+        println()
+    }
+}
+
+
+fun initialiseBoard(input: List<String>): Pair<MutableList<MutableList<Char>>, MutableList<Piece>> {
+    val board = mutableListOf<MutableList<Char>>()
+    val pieces = mutableListOf<Piece>()
+
+    for (i in 0 until input.size) {
+        board.add(mutableListOf())
+        for (j in 0 until input[0].length) {
+            var current = input[i][j]
+
+            if (current == 'E' || current == 'G') {
+                pieces.add(Piece(current, 200, i, j))
+                current = '.'
+            }
+
+            board[i].add(current)
+        }
+    }
+    return Pair(board, pieces)
+}
+
+data class Piece(val Type: Char, var HP: Int, var Row: Int, var Col: Int)
